6a7b02147149726d22c5b6f51a73691e4dd3ecc8
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-10-20 23:54:56 +0300

    theme configs at main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_groceries_app/common/color/color_extension.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Online Groceries',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        fontFamily: "Gilroy",
+        colorScheme: ColorScheme.fromSeed(seedColor: TColor.primary),
+        useMaterial3: false,
       ),
-      home: null,
+      home: Container(),
     );
   }
 }
