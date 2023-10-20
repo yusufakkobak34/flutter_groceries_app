c053fbcc81367986bc23023c4024d7222a8d6e51
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-10-21 00:02:53 +0300

    splash view integrations at main.dart file ü

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_groceries_app/common/color/color_extension.dart';
+import 'package:flutter_groceries_app/view/splash/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Online Groceries',
       theme: ThemeData(
         fontFamily: "Gilroy",
         colorScheme: ColorScheme.fromSeed(seedColor: TColor.primary),
         useMaterial3: false,
       ),
-      home: Container(),
+      home: const SplashView(),
     );
   }
 }
