b8d322669bc5959cae31686285e83becb4a4460a
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-10-20 23:53:31 +0300

    re usable color strings added.

---
+++ lib/common/color/color_extension.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TColor {
+  static Color get primary => const Color(0XFF53B175);
+  static Color get primaryText => const Color(0XFF030303);
+  static Color get secondaryText => const Color(0XFF828282);
+  static Color get textTitle => const Color(0XFF7C7C7C);
+  static Color get placeholder => const Color(0XFFB1B1B1);
+}
