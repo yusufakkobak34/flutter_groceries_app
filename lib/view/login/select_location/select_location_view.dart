82725957e1b8e1e1f00a5b29438b841215811117
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-10-26 20:41:29 +0300

    select location view generated.

---
+++ lib/view/login/select_location/select_location_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SelectLocationView extends StatefulWidget {
+  const SelectLocationView({super.key});
+
+  @override
+  State<SelectLocationView> createState() => _SelectLocationViewState();
+}
+
+class _SelectLocationViewState extends State<SelectLocationView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+    // breakpoint here.
+  }
+}
