5727fd1d32565a5e2ecf6fefd1dcd547e77fc463
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-10-30 01:13:37 +0300

    section view element added.

---
+++ lib/common_widgets/section_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_groceries_app/common/color/color_extension.dart';
+
+class SectionView extends StatelessWidget {
+  final String title;
+  final bool isShowSeeAllButton;
+  final VoidCallback onPressed;
+  final EdgeInsets? padding;
+  const SectionView(
+      {super.key,
+      required this.title,
+      required this.isShowSeeAllButton,
+      required this.onPressed,
+      this.padding});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: TColor.primaryText,
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          TextButton(
+            onPressed: onPressed,
+            child: Text(
+              "Hepsini Gör",
+              style: TextStyle(
+                color: TColor.primary,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
