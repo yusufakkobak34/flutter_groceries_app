600c7c390548e165784f4b254d6a0af1d4df7523
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-10-30 01:24:33 +0300

    product cell added.

---
+++ lib/common_widgets/product_cell.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_groceries_app/common/color/color_extension.dart';
+
+class ProductCell extends StatelessWidget {
+  final Map pObj;
+  final VoidCallback onPressed;
+  const ProductCell({super.key, required this.pObj, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        width: 180,
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: TColor.placeholder.withOpacity(0.5), width: 1),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              pObj['icon'],
+              width: 100,
+              height: 75,
+              fit: BoxFit.contain,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
