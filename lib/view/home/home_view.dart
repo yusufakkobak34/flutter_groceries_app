40080a9ef159c35872a4f7fb1b67a68535808024
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-10-30 00:46:56 +0300

    example datasets added.

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -10,7 +10,70 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   TextEditingController txtSearch = TextEditingController();
-  bool isShow = false;
+
+  List exclusiveOfferArr = [
+    {
+      "name": "Organik Muz",
+      "icon": "assets/img/banana.png",
+      "qty": "7",
+      "unit": "adet,fiyat",
+      "price": "\$1.99"
+    },
+    {
+      "name": "Kırmızı Elma",
+      "icon": "assets/img/apple.png",
+      "qty": "7",
+      "unit": "adet,fiyat",
+      "price": "\$1.99"
+    },
+  ];
+
+  List bestSellingArr = [
+    {
+      "name": "Acı Biber",
+      "icon": "assets/img/bell_pepper_red.png",
+      "qty": "1",
+      "unit": "gram,fiyat",
+      "price": "\$2.99"
+    },
+    {
+      "name": "Zencefil",
+      "icon": "assets/img/ginger.png",
+      "qty": "250",
+      "unit": "gram,fiyat",
+      "price": "\$3.99"
+    }
+  ];
+
+  List groceriesArr = [
+    {
+      "name": "Bakliyat",
+      "icon": "assets/img/pulses.png",
+      "color": Color(0xffF8A44C),
+    },
+    {
+      "name": "Pirinç",
+      "icon": "assets/img/pulses.png",
+      "color": Color(0xff53B175),
+    }
+  ];
+
+  List listArr = [
+    {
+      "name": "Biftek",
+      "icon": "assets/img/beef_bone.png",
+      "qty": "1",
+      "unit": "kg,fiyat",
+      "price": "\$4.99"
+    },
+    {
+      "name": "Tavuk",
+      "icon": "assets/img/broiler_chicken.png",
+      "qty": "1",
+      "unit": "kg,fiyat",
+      "price": "\$4.99"
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
