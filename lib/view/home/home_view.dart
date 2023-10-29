b1d00b3f188d72d1fa6fec4151b8c141c92383f7
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-10-29 20:24:23 +0300

    home view designs.

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_groceries_app/common/color/color_extension.dart';
+import 'package:flutter_groceries_app/common_widgets/line_textfield.dart';
+import 'package:flutter_groceries_app/common_widgets/round_button.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -8,8 +11,67 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  TextEditingController txtSearch = TextEditingController();
+  bool isShow = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var media = MediaQuery.sizeOf(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Image.asset(
+            "assets/img/back.png",
+            width: 20,
+            height: 20,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/img/color_logo.png", width: 40),
+                  ],
+                ),
+                SizedBox(
+                  height: media.width * 0.05,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/img/location.png",
+                      width: 15,
+                      height: 15,
+                    ),
+                    Text(
+                      "Giriş Yapın",
+                      style: TextStyle(
+                        color: TColor.primaryText,
+                        fontSize: 26,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
