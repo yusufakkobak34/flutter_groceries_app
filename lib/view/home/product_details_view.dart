31f4d9e60ea164772f96aaf2f56b8f804f68f238
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-10 01:32:52 +0300

    Add product details view UI elements

--- lib/view/home/product_details_view.dart
+++ lib/view/home/product_details_view.dart
@@ -104,6 +104,186 @@ class _ProductDetailsState extends State<ProductDetails> {
                   const SizedBox(
                     height: 15,
                   ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Image.asset(
+                            "assets/img/subtack.png",
+                            width: 20,
+                            height: 20,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 45,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: TColor.placeholder.withOpacity(0.5),
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "1",
+                          style: TextStyle(
+                            color: TColor.primaryText,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Image.asset(
+                            "assets/img/add_green.png",
+                            width: 20,
+                            height: 20,
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      Text(
+                        "\$ 4.99",
+                        style: TextStyle(
+                          color: TColor.primaryText,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Divider(
+                    color: Colors.black26,
+                    height: 1,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "Ürün Açıklaması",
+                          style: TextStyle(
+                            color: TColor.primaryText,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.asset(
+                          "assets/img/detail_open.png",
+                          width: 15,
+                          height: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                    style: TextStyle(
+                      color: TColor.secondaryText,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Divider(
+                    color: Colors.black26,
+                    height: 1,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "Beslenme",
+                          style: TextStyle(
+                            color: TColor.primaryText,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 4, horizontal: 8),
+                        height: 25,
+                        decoration: BoxDecoration(
+                          color: TColor.placeholder.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "100gr",
+                          style: TextStyle(
+                            color: TColor.secondaryText,
+                            fontSize: 9,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.asset(
+                          "assets/img/next.png",
+                          width: 15,
+                          height: 15,
+                          color: TColor.primaryText,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Divider(
+                    color: Colors.black26,
+                    height: 1,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "İnceleme",
+                          style: TextStyle(
+                            color: TColor.primaryText,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.asset(
+                          "assets/img/next.png",
+                          width: 15,
+                          height: 15,
+                          color: TColor.primaryText,
+                        ),
+                      ),
+                    ],
+                  ),
                   RoundButton(
                     title: "Sepete Ekle",
                     onPressed: () {},
