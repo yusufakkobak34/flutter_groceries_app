2007ae981f195976fd7f18ae9aa42883817b7c63
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-13 01:20:02 +0300

    seperated filter row.

---
+++ lib/common_widgets/filter_row.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_groceries_app/common/color/color_extension.dart';
+
+class FilterRow extends StatelessWidget {
+  final Map fObj;
+  final bool isSelect;
+  final VoidCallback onPressed;
+  const FilterRow(
+      {super.key,
+      required this.fObj,
+      required this.isSelect,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          children: [
+            Image.asset(
+              isSelect
+                  ? "assets/img/checkbox_check.png"
+                  : "assets/img/checkbox.png",
+              width: 25,
+              height: 25,
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            Expanded(
+              child: Text(
+                fObj["name"],
+                style: TextStyle(
+                  color: isSelect ? TColor.primary : TColor.primaryText,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
