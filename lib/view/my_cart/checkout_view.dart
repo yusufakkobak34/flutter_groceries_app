4dd1a1f5cf3ab937a39dac81ffda30f71612dfaa
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-14 02:14:39 +0300

    checkout view file generated.

---
+++ lib/view/my_cart/checkout_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CheckoutView extends StatefulWidget {
+  const CheckoutView({super.key});
+
+  @override
+  State<CheckoutView> createState() => _CheckoutViewState();
+}
+
+class _CheckoutViewState extends State<CheckoutView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
