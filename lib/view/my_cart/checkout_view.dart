1b33b15878e380da94dd2bb39ee0d4fa6a2d6370
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-15 00:37:33 +0300

    Add checkout row and update payment section in checkout view

--- lib/view/my_cart/checkout_view.dart
+++ lib/view/my_cart/checkout_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_groceries_app/common/color/color_extension.dart';
+import 'package:flutter_groceries_app/common_widgets/checkout_row.dart';
 import 'package:flutter_groceries_app/common_widgets/round_button.dart';
 
 class CheckoutView extends StatefulWidget {
@@ -52,43 +53,94 @@ class _CheckoutViewState extends State<CheckoutView> {
             color: Colors.black26,
             height: 1,
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 15),
-            child: Row(
-              children: [
-                Text(
-                  "Teslimat",
-                  style: TextStyle(
-                    color: TColor.secondaryText,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                  ),
+          CheckoutRow(
+            title: "Teslimat",
+            value: "Yöntem Seçin",
+            onPressed: () {},
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: Row(
+                  children: [
+                    Text(
+                      "Ödeme",
+                      style: TextStyle(
+                        color: TColor.secondaryText,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const Spacer(),
+                    Image.asset(
+                      "assets/img/next.png",
+                      height: 15,
+                      color: TColor.primaryText,
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: Text(
-                    "Yöntem Seç",
-                    textAlign: TextAlign.end,
+              ),
+              const Divider(
+                color: Colors.black26,
+                height: 1,
+              ),
+            ],
+          ),
+          CheckoutRow(
+            title: "İndirim Kodu",
+            value: "Kod Girin",
+            onPressed: () {},
+          ),
+          CheckoutRow(
+            title: "Toplam Tutar",
+            value: "\$10.96",
+            onPressed: () {},
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: TColor.secondaryText,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+                children: [
+                  const TextSpan(
+                    text:
+                        "Devam ederek kullanıcı şunları kabul etmiş olursunuz\n",
+                  ),
+                  TextSpan(
+                    text: "Kullanıcı",
                     style: TextStyle(
                       color: TColor.primaryText,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Image.asset(
-                    "assets/img/next.png",
-                    height: 15,
-                    color: TColor.primaryText,
+                  const TextSpan(
+                    text: " ve ",
                   ),
-                )
-              ],
+                  TextSpan(
+                    text: "Gizlilik Sözleşmeleri",
+                    style: TextStyle(
+                      color: TColor.primaryText,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           RoundButton(
             title: "Sipariş Et",
             onPressed: () {},
+          ),
+          const SizedBox(
+            height: 15,
           )
         ],
       ),
