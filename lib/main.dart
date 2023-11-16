f493dff8788440d96e91cd338f1ebc2470e02645
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-16 15:21:11 +0300

    ios pod configs and easyloading config.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,32 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_groceries_app/common/color/color_extension.dart';
+import 'package:flutter_groceries_app/common/http/http_overrides.dart';
 import 'package:flutter_groceries_app/view/maintab_view/main_tab_view.dart';
+import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 SharedPreferences? prefs;
-void main() {
+void main() async {
+  HttpOverrides.global = MyHttpOverrides();
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
   runApp(const MyApp());
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..indicatorType = EasyLoadingIndicatorType.ring
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..indicatorSize = 45.0
+    ..radius = 5.0
+    ..progressColor = TColor.primaryText
+    ..backgroundColor = TColor.primary
+    ..indicatorColor = Colors.yellow
+    ..textColor = TColor.primaryText
+    ..userInteractions = false
+    ..dismissOnTap = false;
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +34,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Online Groceries',
       theme: ThemeData(
@@ -25,4 +46,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
