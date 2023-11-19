cd7b1da607ae0185e2683bd2b2e71bbfb6016062
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-19 20:08:35 +0300

    user payload model added.

---
+++ lib/model/user/user_payload_model.dart
@@ -0,0 +1,24 @@
+class UserPayloadModel {
+  int? userId;
+  String? username;
+  String? name;
+  String? email;
+  String? mobile;
+  String? mobileCode;
+  String? password;
+  String? authToken;
+  int? status;
+  String? createdDate;
+  UserPayloadModel({
+    this.userId,
+    this.username,
+    this.name,
+    this.email,
+    this.mobile,
+    this.mobileCode,
+    this.password,
+    this.authToken,
+    this.status,
+    this.createdDate,
+  });
+}
