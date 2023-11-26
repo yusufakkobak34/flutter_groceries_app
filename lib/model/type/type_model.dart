80c9af9085307cba706fb3c48f1c0a9bb53f8671
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-27 00:46:04 +0300

    Fix color conversion in TypeModel

--- lib/model/type/type_model.dart
+++ lib/model/type/type_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_groceries_app/common/color/color_extension.dart';
 
 class TypeModel {
   int? typeId;
@@ -17,7 +18,7 @@ class TypeModel {
     typeId = json['type_id'];
     typeName = json['type_name'];
     image = json['image'];
-    color = json['color'];
+    color = HexColor.fromHex(json['color'].toString());
   }
 
   Map<String, dynamic> toJson() {
@@ -25,7 +26,7 @@ class TypeModel {
     data['type_id'] = typeId;
     data['type_name'] = typeName;
     data['image'] = image;
-    data['color'] = color;
+    data['color'] = color?.toHex();
     return data;
   }
 }
