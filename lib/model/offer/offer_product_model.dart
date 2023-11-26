f11e30f18dbd163671c0aff4112e795ff3a27a83
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-27 00:34:25 +0300

    Add fromJson and toJson methods to OfferProductModel

--- lib/model/offer/offer_product_model.dart
+++ lib/model/offer/offer_product_model.dart
@@ -35,5 +35,43 @@ class OfferProductModel {
     this.typeName,
   });
 
+  OfferProductModel.fromJson(Map<String, dynamic> json) {
+    offerPrice = json['offer_price'];
+    startDate = json['start_date'];
+    endDate = json['end_date'];
+    prodId = json['prod_id'];
+    catId = json['cat_id'];
+    brandId = json['brand_id'];
+    typeId = json['type_id'];
+    name = json['name'];
+    detail = json['detail'];
+    unitName = json['unit_name'];
+    unitValue = json['unit_value'];
+    nutritionWeight = json['nutrition_weight'];
+    price = json['price'];
+    image = json['image'];
+    catName = json['cat_name'];
+    typeName = json['type_name'];
+  }
 
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['offer_price'] = offerPrice;
+    data['start_date'] = startDate;
+    data['end_date'] = endDate;
+    data['prod_id'] = prodId;
+    data['cat_id'] = catId;
+    data['brand_id'] = brandId;
+    data['type_id'] = typeId;
+    data['name'] = name;
+    data['detail'] = detail;
+    data['unit_name'] = unitName;
+    data['unit_value'] = unitValue;
+    data['nutrition_weight'] = nutritionWeight;
+    data['price'] = price;
+    data['image'] = image;
+    data['cat_name'] = catName;
+    data['type_name'] = typeName;
+    return data;
+  }
 }
