d10af063a5dd6fd0452c68e58f962b16f8538d23
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-27 00:23:34 +0300

    type model added.

---
+++ lib/model/type/type_model.dart
@@ -0,0 +1,29 @@
+class TypeModel {
+  int? typeId;
+  String? typeName;
+  String? image;
+  String? color;
+
+  TypeModel({
+    this.typeId,
+    this.typeName,
+    this.image,
+    this.color,
+  });
+
+  TypeModel.fromJson(Map<String, dynamic> json) {
+    typeId = json['type_id'];
+    typeName = json['type_name'];
+    image = json['image'];
+    color = json['color'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['type_id'] = typeId;
+    data['type_name'] = typeName;
+    data['image'] = image;
+    data['color'] = color;
+    return data;
+  }
+}
