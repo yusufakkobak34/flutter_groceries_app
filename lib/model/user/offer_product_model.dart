6f7d48978e1ab824b351cb53f4deccc2275e3d12
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-11-27 00:20:21 +0300

    offer product model added.

---
+++ lib/model/user/offer_product_model.dart
@@ -0,0 +1,39 @@
+class OfferProductModel {
+  double? offerPrice;
+  String? startDate;
+  String? endDate;
+  int? prodId;
+  int? catId;
+  int? brandId;
+  int? typeId;
+  String? name;
+  String? detail;
+  String? unitName;
+  String? unitValue;
+  String? nutritionWeight;
+  double? price;
+  String? image;
+  String? catName;
+  String? typeName;
+
+  OfferProductModel({
+    this.offerPrice,
+    this.startDate,
+    this.endDate,
+    this.prodId,
+    this.catId,
+    this.brandId,
+    this.typeId,
+    this.name,
+    this.detail,
+    this.unitName,
+    this.unitValue,
+    this.nutritionWeight,
+    this.price,
+    this.image,
+    this.catName,
+    this.typeName,
+  });
+
+
+}
